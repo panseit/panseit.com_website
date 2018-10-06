848058caf9f885026ddeb1cbeb77d2893eb5c150
Panagiotis Seitanidis <panseit@gmail.com>
2018-10-06 16:33:45 +0300

    Fix for Background Blur and Responsiveness problems

--- js/main.js
+++ js/main.js
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "Why are we not online yet?Prolly busy :)";
+    document.getElementById("countdown").innerHTML = "I am working hard to be online soon";
   }
 }, 1000);
