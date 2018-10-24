430d260c53d6745e5329f41cac6c6da5f9b9f7cc
Panagiotis Seitanidis <panseit@gmail.com>
2018-10-24 16:57:01 +0300

    Small fixes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var countDownDate = new Date("Jan 10, 2019 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
   // Get todays date and time
   var now = new Date().getTime();
@@ -17,8 +17,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in an element with id="countdown"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h " +
+    minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
